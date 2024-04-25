19eac9915734e5179393c3d3adbfd857b4610e4d
dante-signal31 <dante.signal31@gmail.com>
2024-04-25 20:16:43 +0200

    Hurtboxes and Hitboxes created.

---
+++ Scripts/Characters/Hurtbox.cs
@@ -0,0 +1,28 @@
+﻿using Godot;
+
+namespace DungeonRPG.Scripts.Characters;
+
+public partial class Hurtbox : Area3D
+{
+    public override void _EnterTree()
+    {
+        AreaEntered += OnAreaEntered;
+        AreaExited += OnAreaExited;
+    }
+
+    public override void _ExitTree()
+    {
+        AreaEntered -= OnAreaEntered;
+        AreaExited -= OnAreaExited;
+    }
+
+    private void OnAreaEntered(Area3D area)
+    {
+        GD.Print($"[{GetOwner<Node>().Name}] Under attack from {area.GetOwner<Node>().Name}");
+    }
+
+    private void OnAreaExited(Area3D area)
+    {
+        GD.Print($"[{GetOwner<Node>().Name}] {area.GetOwner<Node>().Name} ceases his attack.");
+    }
+}
