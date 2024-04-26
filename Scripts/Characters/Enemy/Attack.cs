4804dbfad7673c1207f8365bd143749ab8fd15a5
dante-signal31 <dante.signal31@gmail.com>
2024-04-26 18:30:29 +0200

    First working version of enemy attack.

---
+++ Scripts/Characters/Enemy/Attack.cs
@@ -0,0 +1,23 @@
+﻿using Godot;
+
+namespace DungeonRPG.Scripts.Characters.Enemy;
+
+public partial class Attack : Node
+{
+    [ExportCategory("WIRING:")]
+    [Export] private Enemy _characterNode;
+    
+    [ExportCategory("CONFIGURATION:")] 
+    [Export] private float _hitBoxDistance = 1.0f;
+    
+    private void PerformHit()
+    {
+        PlaceHitBox();
+    }
+
+    private void PlaceHitBox()
+    {
+        Vector3 newPosition = _characterNode.IsFacingLeft? Vector3.Left : Vector3.Right;
+        _characterNode.HitBox.Position = newPosition * _hitBoxDistance;
+    }
+}
---
+++ Scripts/Characters/Enemy/Combat.cs
@@ -0,0 +1,43 @@
+﻿using DungeonRPG.Scripts.Sensors;
+using Godot;
+
+namespace DungeonRPG.Scripts.Characters.Enemy;
+
+public partial class Combat : Node
+{
+    [ExportCategory("WIRING:")] [Export] private Enemy _characterNode;
+    [Export] private VolumetricSensor _attackSensor;
+    [Export] private StateMachine _stateMachine;
+
+    /// <summary>
+    /// Whether the attack sensor is pointing to left or not.
+    /// </summary>
+    private bool AttackSensorFacingLeft => _attackSensor.RotationDegrees.Y == 180;
+
+    private void HitPlayer()
+    {
+        _stateMachine?.SwitchState<Attack>();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if (_attackSensor.DetectedBodies.Count > 0)
+        {
+            HitPlayer();
+        }
+
+        FlipAttackSensorIfNeeded();
+    }
+
+    private void FlipAttackSensorIfNeeded()
+    {
+        if (_characterNode.IsFacingLeft != AttackSensorFacingLeft)
+        {
+            _attackSensor.RotationDegrees = _characterNode.IsFacingLeft ?
+                _attackSensor.RotationDegrees with { Y = 180} :
+                _attackSensor.RotationDegrees with { Y = 0};
+        }
+    }
+}
+
+
