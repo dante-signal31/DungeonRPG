06821a4b50ab5252433d07b77d6a2e099267246d
dante-signal31 <dante.signal31@gmail.com>
2024-05-05 01:30:56 +0200

    Minimap ready.

---
+++ Scripts/General/MiniMapCamera.cs
@@ -0,0 +1,15 @@
+using DungeonRPG.Scripts.Characters.Player;
+using Godot;
+
+namespace DungeonRPG.Scripts.General;
+
+public partial class MiniMapCamera : Camera3D
+{
+    [ExportCategory("WIRING:")] 
+    [Export] private Player _player;
+
+    public override void _Process(double delta)
+    {
+        Position = _player.Position with {Y = Position.Y};
+    }
+}
